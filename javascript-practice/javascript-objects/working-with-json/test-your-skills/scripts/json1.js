d61b385094a6949a8d691c5c7fb3d4ac9af559c9
varun <singhvarun928@gmail.com>
2023-06-21 13:08:52 +0530

    completed working with json section

--- javascript-practice/javascript-objects/working-with-json/test-your-skills/scripts/json1.js
+++ javascript-practice/javascript-objects/working-with-json/test-your-skills/scripts/json1.js
@@ -1,26 +1,46 @@
 const section = document.querySelector('section');
 
-    let para1 = document.createElement('p');
-    let para2 = document.createElement('p');
-    let motherInfo = 'The mother cats are called ';
-    let kittenInfo;
-    const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json';
+let para1 = document.createElement('p');
+let para2 = document.createElement('p');
+let motherInfo = 'The mother cats are called ';
+let kittenInfo;
+const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json';
 
-    fetch(requestURL)
-    .then(response => response.text())
-    .then(text => displayCatInfo(text))
+fetch(requestURL)
+  .then(response => response.text())
+  .then(text => displayCatInfo(text))
 
-    function displayCatInfo(catString) {
-      let total = 0;
-      let male = 0;
+function displayCatInfo(catString) {
+  let total = 0;
+  let male = 0;
 
-      // Add your code here
+  const catObj = JSON.parse(catString);
+  // console.log(catObj.length);
+  // console.log(catObj[1]["name"]);
 
-      // Don't edit the code below here!
-
-      para1.textContent = motherInfo;
-      para2.textContent = kittenInfo;
+  for(let i = 0; i < catObj.length; i++){
+    if(i === catObj.length - 1){
+      motherInfo += `and ${catObj[i]["name"]}.`;
+    }else{
+      motherInfo += `${catObj[i]["name"]}, `;
     }
 
-    section.appendChild(para1);
-    section.appendChild(para2);
+    const kittens = catObj[i].kittens;
+    // console.log(kittens);
+    total += kittens.length;
+    // console.log(total)
+    for(kitten of kittens){
+      if(kitten.gender === 'm')
+        male += 1;
+    }
+  }
+
+  // console.log(total, male, total - male);
+  kittenInfo = `Total number of kittens are ${total}, where male are ${male} and female are ${total - male}.`;
+  
+  para1.textContent = motherInfo;
+  para2.textContent = kittenInfo;
+}
+
+section.appendChild(para1);
+section.appendChild(para2);

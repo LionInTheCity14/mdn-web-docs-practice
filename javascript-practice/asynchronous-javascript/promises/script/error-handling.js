2094769686e650f0a35328849240b939c2cc8a26
varun <singhvarun928@gmail.com>
2023-07-03 09:19:59 +0530

    completed till combining multiple promises

--- javascript-practice/asynchronous-javascript/promises/script/error-handling.js
+++ javascript-practice/asynchronous-javascript/promises/script/error-handling.js
@@ -17,16 +17,66 @@ const fetchPromise = fetch(
 
 fetchPromise
   .then((response) => {
-    if(!response.ok){
-      throw new Error(`HTTP error: ${response.status}`);
-    }
+    if (!response.ok)
+      throw new Error(`HTTP Error occured : ${response.status}`)
     return response.json();
   })
-  .then((data) => {
-    console.log(data[0].name);
+  .then((data) => console.log(data[0].name))
+  .catch((error) => console.error(`Could not get products: "${error}"`));
+
+// Combining multiple promises using Promise.all()
+
+// The promise returned by Promise.all() is:
+
+// 1) fulfilled when and if all the promises in the array are
+// fulfilled. In this case, the then() handler is called with
+// an array of all the responses, in the same order that the
+// promises were passed into all().
+
+// 2) rejected when and if any of the promises in the array
+// are rejected. In this case, the catch() handler is called
+// with the error thrown by the promise that rejected.
+
+const fetchPromise1 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+const fetchPromise2 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found"
+);
+const fetchPromise3 = fetch(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+);
+
+Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then((responses) => {
+    for (response of responses) {
+      console.log(`${response.url}: ${response.status}`);
+    }
   })
-  .catch((error) => {
-    console.error(`Could not get products: "${error}"`);
-  });
+  .catch((error) => console.error(`Failed to fetch : ${error}`));
+
+// Sometimes, you might need any one of a set of promises to be
+// fulfilled, and don't care which one. In that case, you want
+// Promise.any(). This is like Promise.all(), except that it is
+// fulfilled as soon as any of the array of promises is 
+// fulfilled, or rejected if all of them are rejected:
+
+const fetchPromise4 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+const fetchPromise5 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found"
+);
+const fetchPromise6 = fetch(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+);
+
+Promise.any([fetchPromise4, fetchPromise5, fetchPromise6])
+  .then((responses) => {
+    for(response of responses){
+      console.log(`${response.url} : ${response.status}`);
+    }
+  })
+  .catch((error) => console.error(`Failed to fetch: "${error}"`));
 
   

8084b37e1a706b797f412b3258992e678449e0e3
varun <singhvarun928@gmail.com>
2023-07-09 23:10:52 +0530

    Added some code in audio-api.js

--- javascript-practice/client-side-web-apis/introduction-to-web-apis/scripts/audio-api.js
+++ javascript-practice/client-side-web-apis/introduction-to-web-apis/scripts/audio-api.js
@@ -1,2 +1,6 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
+
+const audioElement = document.querySelector("audio");
+const playBtn = document.querySelector("button");
+const volumeSlider = audioCtx.createMediaElementSource(audioElement);

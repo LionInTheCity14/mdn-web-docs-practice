969a3905a2c0b4cf33280cdf41ca93c5800c95d4
varun <singhvarun928@gmail.com>
2023-07-24 22:38:43 +0530

    continuing third party apis

--- javascript-practice/client-side-web-apis/third-party-apis/mapquest/scripts/main.js
+++ javascript-practice/client-side-web-apis/third-party-apis/mapquest/scripts/main.js
@@ -4,3 +4,9 @@ const audioElement = document.querySelector("audio");
 // ...
 const audioSource = audioCtx.createMediaElementSource(audioElement);
 // etc.
+
+const map = L.mapquest.map("map", {
+    center: [53.480759, -2.242631],
+    layers: L.mapquest.tileLayer("map"),
+    zoom: 12,
+});
